eae568a8012e5b484b508835f38ae919efe939b4
Austin kabi@ <kabimuriithi@gmail.com>
2024-03-22 14:00:00 +0300

    addition

---
+++ add_no.py
@@ -0,0 +1,5 @@
+def add_numbers(a,b,c):
+    sum=a+b+c
+    print("{} + {} + {} = {}".format(a,b,c,sum))
+add_numbers(10,6,2)
+add_numbers(10,10,10)
---
+++ def.py
@@ -0,0 +1,6 @@
+def name_age(name,age):
+    print(f"name:{name} and {age} ")
+name_age("silas",34)
+name_age("Austin",30)
+name_age("James",55)
+name_age("John",40)
